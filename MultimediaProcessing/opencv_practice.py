50e9103d8ce01c527c49ebe4962c1e4213fb2dd5
XinzheTing <tingxinzhe1@gmail.com>
2018-10-23 17:44:45 +0800

    10-23

--- MultimediaProcessing/opencv_practice.py
+++ MultimediaProcessing/opencv_practice.py
@@ -3,12 +3,16 @@
 
 
 import cv2
-
-c = cv2.VideoCapture(0)
+import os
+module_path = os.path.dirname(__file__)
+filein = module_path + '/square_detection/data2.avi'
+c = cv2.VideoCapture(filein)
 while(c.isOpened()):
     ret, frame = c.read()
-    cv2.imshow('1', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if not ret:
         break
+    cv2.imshow('1', frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 c.release()
 cv2.destroyAllWindows() 
